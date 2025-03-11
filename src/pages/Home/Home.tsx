3a49cde34d437627e060743a4bd27915b233f6fb
daeseopk <eotjqdlee@gmail.com>
2025-03-11 13:06:22 +0900

    feat : modify home style

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Header from "../../components/composite/Header/Header";
 import { TABS } from "../../dummy/dummy";
+import "./style.scss";
 
 export default function Home() {
   return (
-    <div>
+    <div className="home-container">
       <Header tabs={TABS} />
       <Outlet />
     </div>
