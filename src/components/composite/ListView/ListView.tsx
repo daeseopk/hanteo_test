d2684bea1de89f0377ee5d81795dea5122cc28f1
daeseopk <eotjqdlee@gmail.com>
2025-03-12 13:51:39 +0900

    feat : add ListView

---
+++ src/components/composite/ListView/ListView.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+import "./style.scss";
+
+interface Props {
+  title: string;
+  children: ReactNode;
+}
+
+export default function ListView(props: Props) {
+  const { title, children } = props;
+  return (
+    <div className="list-view-container">
+      <span className="list-view-title">{title}</span>
+      <div className="list-view-content-wrapper">{children}</div>
+    </div>
+  );
+}
--- src/pages/Chart/Chart.tsx
+++ src/pages/Chart/Chart.tsx
@@ -1,5 +1,6 @@
 import BannerCard from "../../components/composite/BannerCard/BannerCard";
 import Carousel from "../../components/composite/Carousel/Carousel";
+import ListView from "../../components/composite/ListView/ListView";
 import { BANNERS } from "../../dummy/dummy";
 import { BannerItem } from "../../types/banner";
 import "./style.scss";
@@ -11,6 +12,9 @@ export default function Chart() {
         items={BANNERS}
         renderItem={(item) => <BannerCard item={item} />}
       />
+      <div className="list-view-wrapper">
+        <ListView title="콘텐츠 큐레이션 제목">ㅁㄴㅇㄹ</ListView>
+      </div>
     </div>
   );
 }
