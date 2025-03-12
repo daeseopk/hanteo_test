6cfdb47f5104d52b12924ce7388c1ec89702a4bc
daeseopk <eotjqdlee@gmail.com>
2025-03-12 14:09:38 +0900

    init postcard

---
+++ src/components/composite/PostCard/PostCard.tsx
@@ -0,0 +1,10 @@
+import { Post } from "../../../types/post";
+import Card from "../../base/Card/Card";
+
+export default function PostCard(props: Post) {
+  return (
+    <Card width="100%" height="40px">
+      PostCard
+    </Card>
+  );
+}
