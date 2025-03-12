5e757148b3b8798668f335661706ba223026f1c7
daeseopk <eotjqdlee@gmail.com>
2025-03-12 13:25:52 +0900

    feat : type definition for useGetPostList

--- src/queries/useGetPostList.ts
+++ src/queries/useGetPostList.ts
@@ -1,2 +1,9 @@
-interface Params {}
-export default function useGetPostList() {}
+type Params = {
+  limit: number;
+  page: number;
+};
+interface Props {
+  params: Params;
+  setParams: (params: Params) => void;
+}
+export default function useGetPostList(props: Props) {}
