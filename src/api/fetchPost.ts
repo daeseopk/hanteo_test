831c1dc1af6dbaa737f69bedf96823115df6bfed
daeseopk <eotjqdlee@gmail.com>
2025-03-12 14:58:44 +0900

    add hasNext function on fetchPost

--- src/api/fetchPost.ts
+++ src/api/fetchPost.ts
@@ -1,11 +1,18 @@
 import { POSTS } from "../dummy/dummy";
 import { Params } from "../types/params";
 import { Post } from "../types/post";
+import { Response } from "../types/response";
 
-export const fetchPosts = async (params: Params): Promise<Array<Post>> => {
+const INCREMENTAL_LIMIT = 10;
+
+export const fetchPosts = async (params: Params): Promise<Response<Post>> => {
   const { limit, page } = params;
   const startIndex = (page - 1) * limit;
-  const endIndex = startIndex + limit;
+  const endIndex = startIndex + limit + INCREMENTAL_LIMIT;
+
+  const checkHasNext = () => {
+    return endIndex < POSTS.length;
+  };
 
   // 실제 API 호출 대신 더미 데이터 슬라이싱
   const paginatedPosts = POSTS.slice(startIndex, endIndex);
@@ -13,5 +20,8 @@ export const fetchPosts = async (params: Params): Promise<Array<Post>> => {
   // 네트워크 지연 시뮬레이션
   await new Promise((resolve) => setTimeout(resolve, 500));
 
-  return paginatedPosts;
+  return {
+    data: paginatedPosts,
+    hasNext: checkHasNext(),
+  };
 };
